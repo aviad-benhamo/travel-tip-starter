30ec8a607d629c81e44f959491601c77c89baf3b
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-11-19 12:58:08 +0200

    feat: add confirmation dialog before removing location

--- js/app.controller.js
+++ js/app.controller.js
@@ -69,6 +69,9 @@ function renderLocs(locs) {
 }
 
 function onRemoveLoc(locId) {
+    let userConfirm = confirm("Are you sure?")
+    if (!userConfirm) return
+
     locService.remove(locId)
         .then(() => {
             flashMsg('Location removed')
@@ -223,7 +226,7 @@ function getFilterByFromQueryParams() {
     const queryParams = new URLSearchParams(window.location.search)
     const txt = queryParams.get('txt') || ''
     const minRate = queryParams.get('minRate') || 0
-    locService.setFilterBy({txt, minRate})
+    locService.setFilterBy({ txt, minRate })
 
     document.querySelector('input[name="filter-by-txt"]').value = txt
     document.querySelector('input[name="filter-by-rate"]').value = minRate
